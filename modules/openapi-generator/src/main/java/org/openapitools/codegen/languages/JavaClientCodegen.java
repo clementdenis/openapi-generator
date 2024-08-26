ce97ddaca5cf090a2fe45622e0a20c1edf74ec3f
clement <clement.denis@gmail.com>
2024-08-27 00:21:46 +0200

    Support for disallowAdditionalPropertiesIfNotPresent on apache-httpclient library

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavaClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/JavaClientCodegen.java
@@ -490,6 +490,13 @@ public class JavaClientCodegen extends AbstractJavaCodegen
 
         if (APACHE.equals(getLibrary()) || RESTTEMPLATE.equals(getLibrary())) {
             supportingFiles.add(new SupportingFile("BaseApi.mustache", invokerFolder, "BaseApi.java"));
+
+            // Composed schemas can have the 'additionalProperties' keyword, as specified in JSON schema.
+            // In principle, this should be enabled by default for all code generators. However due to limitations
+            // in other code generators, support needs to be enabled on a case-by-case basis.
+            // The flag below should be set for all Java libraries, but the templates need to be ported
+            // one by one for each library.
+            supportsAdditionalPropertiesWithComposedSchema = true;
         }
 
         if (FEIGN.equals(getLibrary())) {
